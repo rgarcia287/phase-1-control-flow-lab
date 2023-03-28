bf65162355db2dc1402ae05ea289e027359b803a
Roberto <robertogarcia287@gmail.com>
2023-03-27 20:51:54 -0500

    Roberto's control lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(n){
   // Write your code here!
+    if (n <= 400) {
+      return 'This one is on me!';
+    } else if (400 < n && n < 2000) {
+      return "That will be twenty bucks."
+    } else if (2000 < n && n < 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(a){
   // Write your code here!
+  return a === "NYC"?   `Ok, sounds good.`: `No go.`;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
